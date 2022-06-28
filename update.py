db0c0f6e5fb78fb5135e0cf7060dd98c17bb6b36
sdhtele <97067334+sdhtele@users.noreply.github.com>
2022-06-29 01:27:17 +0700

    Update update.py

--- update.py
+++ update.py
@@ -37,7 +37,7 @@ try:
     if len(UPSTREAM_REPO) == 0:
        raise TypeError
 except:
-    UPSTREAM_REPO = "https://github.com/Dawn-India/Z-Mirror"
+    UPSTREAM_REPO = "https://github.com/sdhtele/zws"
 try:
     if len(UPSTREAM_BRANCH) == 0:
        raise TypeError
